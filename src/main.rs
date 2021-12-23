9c9dc8f509ce7f25cce1db41f4426e43054c5b82
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2021-12-23 16:00:29 +0100

    server accepts single connection, then immediatly closes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listeners = TcpListener::bind("localhost:8080").await.unwrap();
+    let (socket, addr) = listeners.accept().await.unwrap();
+    let mut buffer = [0u8; 1024];
+    socket.read(&mut buffer).await.unwrap();
     println!("Hello, world!");
 }
