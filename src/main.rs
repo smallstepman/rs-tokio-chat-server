2bb9987903944f111b98aefc2bdb050e992b8889
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2021-12-23 16:08:48 +0100

    refactor: using BufReader instead of array u8 buffer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::process::exit;
+
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
 };
 
@@ -7,9 +9,15 @@ use tokio::{
 async fn main() {
     let listeners = TcpListener::bind("localhost:8080").await.unwrap();
     let (mut socket, _addr) = listeners.accept().await.unwrap();
+    let (reader, mut writer) = socket.split();
+    let mut reader = BufReader::new(reader);
+    let mut line = String::new();
     loop {
-        let mut buffer = [0u8; 1024];
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0 {
+            break;
+        }
+        writer.write_all(&line.as_bytes()).await.unwrap();
+        line.clear();
     }
 }
